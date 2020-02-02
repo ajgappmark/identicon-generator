7ecaab24bc0eec7e63804415fca92b2bd7896329
Johannes Unglert <47390613+junglert@users.noreply.github.com>
2020-02-02 22:44:23 +0100

    Update identicongen.sh

--- identicongen.sh
+++ identicongen.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#author: junglert
+#mail:
+#date: 26.04.2019
+#license: GPL-3.0
+#summary: this bash script creates your own identicon
      
 read -p "Loginname or LDAP, etc: " var
            
